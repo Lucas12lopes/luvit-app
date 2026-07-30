4c46bdc06fc15a0b8c244f5fa5acb59f1f880087
Lucas12lopes <lucaslopeschaves1997@gmail.com>
2026-07-30 12:58:38 -0300

    Corrige URL de conexão do Supabase

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 window.LUVIT_CONFIG = {
-  supabaseUrl: "https://rhewtwmqaoirxlbojkcf.supabase.co/rest/v1/",
-  supabaseAnonKey: "sb_publishable_cGHk4ulzzCAjdJssAqesCg_NaF-xH6W",
-  canonicalUrl: "https://seu-dominio.com"
+  supabaseUrl: "https://rhewtwmqaoirxlbojkcf.supabase.co",
+  supabaseAnonKey: "SUA_CHAVE_PUBLICA_ATUAL",
+  canonicalUrl: "https://luvit-app.pages.dev"
 };
